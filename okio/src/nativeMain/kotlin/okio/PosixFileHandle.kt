1cf87db50f864321e7dc71793ff3739bd81c51db
Jesse Wilson <jwilson@squareup.com>
2021-05-15 20:26:33 -0400

    Implement FileHandle for PosixFileSystem

---
+++ okio/src/nativeMain/kotlin/okio/PosixFileHandle.kt
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2021 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package okio
+
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.usePinned
+import platform.posix.FILE
+import platform.posix.errno
+import platform.posix.fclose
+import platform.posix.fflush
+import platform.posix.fileno
+import platform.posix.fstat
+import platform.posix.ftruncate
+import platform.posix.pread
+import platform.posix.pwrite
+import platform.posix.stat
+
+@ExperimentalFileSystem
+internal class PosixFileHandle(
+  readWrite: Boolean,
+  private val file: CPointer<FILE>
+) : FileHandle(readWrite) {
+  override fun size(): Long {
+    memScoped {
+      val stat = alloc<stat>()
+      if (fstat(fileno(file), stat.ptr) != 0) {
+        throw errnoToIOException(errno)
+      }
+      return stat.st_size
+    }
+  }
+
+  override fun protectedRead(
+    fileOffset: Long,
+    array: ByteArray,
+    arrayOffset: Int,
+    byteCount: Int
+  ): Int {
+    val bytesRead = array.usePinned { pinned ->
+      pread(fileno(file), pinned.addressOf(arrayOffset), byteCount.toULong(), fileOffset)
+    }
+    if (bytesRead == -1L) throw errnoToIOException(errno)
+    if (bytesRead == 0L) return -1
+    return bytesRead.toInt()
+  }
+
+  override fun protectedWrite(
+    fileOffset: Long,
+    array: ByteArray,
+    arrayOffset: Int,
+    byteCount: Int
+  ) {
+    val bytesWritten = array.usePinned { pinned ->
+      pwrite(fileno(file), pinned.addressOf(arrayOffset), byteCount.toULong(), fileOffset)
+    }
+    if (bytesWritten.toInt() != byteCount) throw errnoToIOException(errno)
+  }
+
+  override fun protectedFlush() {
+    if (fflush(file) != 0) {
+      throw errnoToIOException(errno)
+    }
+  }
+
+  override fun protectedResize(size: Long) {
+    if (ftruncate(fileno(file), size) == -1) {
+      throw errnoToIOException(errno)
+    }
+  }
+
+  override fun protectedClose() {
+    if (fclose(file) != 0) {
+      throw errnoToIOException(errno)
+    }
+  }
+}
